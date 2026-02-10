0fa953320066565cbe2031aa08bbb7128fc878a3
ToEz608 <Louisboerner@outlook.de>
2026-02-10 08:17:17 +0100

    Codeteilung

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-MENU = {
-    "espresso": {
-        "price": 8.80,
-        "needs": {"water_ml": 50, "milk_ml": 100, "coffee_g": 15}
-    },
-    "cappuccino": {
-        "price": 5.20,
-        "needs": {"water_ml": 50, "milk_ml": 200, "coffee_g": 15} 
-    },
-    "latte": {
-        "price": 4.80,
-        "needs": {"water_ml": 0, "milk_ml": 250, "coffee_g": 25} 
-    }
-}
-
-ACCEPTED_COINS = [0.05, 0.10, 0.20, 1.00, 2.00]
-
-resources = {
-    "water_ml": 150,
-    "milk_ml": 300,
-    "coffee_g": 75,
-    "money_eur": 35.50
-}
-
+from config import MENU, resources, ACCEPTED_COINS
+ 
 # ---------------------
 # Hilfsfunktionen
 # ---------------------
